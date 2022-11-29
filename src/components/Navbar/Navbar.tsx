62ab47a8fc8a556533dc2571a1c4ee59b2004144
eniiku <enikuauth@gmail.com>
2022-11-29 23:16:50 +0100

    feat(Navbar): implement logic for triggering mobile navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import logoIcon from '../../assets/icons/logo.svg';
 import menuIcon from '../../assets/icons/icon-menu.svg';
 import closeMenuIcon from '../../assets/icons/icon-close-menu.svg';
@@ -5,45 +7,58 @@ import closeMenuIcon from '../../assets/icons/icon-close-menu.svg';
 const navLinks = ['features', 'company', 'careers', 'about'];
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleOpenMenu = () => setIsMenuOpen(true);
+  const handleCloseMenu = () => setIsMenuOpen(false);
+
   return (
     <header className='flex items-center justify-between px-4 py-6'>
       <img src={logoIcon} alt='logo icon for snap company' />
 
-      <button aria-label='click to open mobile navbar'>
-        <img src={menuIcon} alt='click to open mobile navbar' />
+      <button aria-label='click to open mobile navbar' onClick={handleOpenMenu}>
+        <img src={menuIcon} role='presentation' />
       </button>
 
-      <div
-        className='fixed z-30 top-0 bottom-0 right-0 w-8/12 bg-almostWhite flex 
+      {isMenuOpen ? (
+        <div
+          className='fixed z-30 top-0 bottom-0 right-0 w-8/12 bg-almostWhite flex 
           flex-col gap-4 p-6 mobile-nav-shadow'
-      >
-        <button className='self-end'>
-          <img src={closeMenuIcon} alt='click to close mobile navbar' />
-        </button>
+        >
+          <button
+            className='self-end'
+            aria-label='click to close mobile navbar'
+            onClick={handleCloseMenu}
+          >
+            <img src={closeMenuIcon} role='presentation' />
+          </button>
 
-        <nav className='mt-2'>
-          <ul>
-            {navLinks.map((navLink) => (
-              <li key={navLink} className='py-2'>
-                <button className='capitalize text-mediumGray'>
-                  {navLink}
-                </button>
-              </li>
-            ))}
-          </ul>
+          <nav className='mt-2'>
+            <ul>
+              {navLinks.map((navLink) => (
+                <li key={navLink} className='py-2'>
+                  <button className='capitalize text-mediumGray'>
+                    {navLink}
+                  </button>
+                </li>
+              ))}
+            </ul>
 
-          <div className='flex flex-col items-center justify-center'>
-            <button className='py-2 text-center text-mediumGray'>Login</button>
+            <div className='flex flex-col items-center justify-center'>
+              <button className='py-2 text-center text-mediumGray'>
+                Login
+              </button>
 
-            <button
-              className='py-2 text-center border-2 border-mediumGray
+              <button
+                className='py-2 text-center border-2 border-mediumGray
               text-mediumGray w-full rounded-2xl'
-            >
-              Register
-            </button>
-          </div>
-        </nav>
-      </div>
+              >
+                Register
+              </button>
+            </div>
+          </nav>
+        </div>
+      ) : null}
     </header>
   );
 };
