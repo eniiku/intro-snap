96c5473cb6d8a369148b0ee8857c0b25f1702cf8
eniiku <enikuauth@gmail.com>
2022-11-30 22:22:48 +0100

    style(DropDown): style dropdown for desktop view

--- src/components/Navbar/DropDown.tsx
+++ src/components/Navbar/DropDown.tsx
@@ -15,20 +15,30 @@ const DropDown = ({
   const handleCloseDropDown = () => setIsOpen(false);
 
   return (
-    <div>
+    <div className='relative'>
       <button
         aria-label='click to open drop-down menu'
         onClick={() => {
           return !isOpen ? handleOpenDropDown() : handleCloseDropDown();
         }}
+        className='capitalize text-mediumGray hover:text-almostBlack'
       >
-        {children} <img src={arrowDownIcon} aria-hidden />
+        {children}
+        <img src={arrowDownIcon} className='inline ml-2' aria-hidden />
       </button>
 
       {isOpen ? (
-        <div>
+        <div
+          className='absolute z-40 shadow-2xl p-3 rounded-xl border border-mediumGray
+          border-opacity-10 -right-4 top-12 bg-white mr-8'
+        >
           {dropDownContent.map((item: any) => (
-            <button key={item.text} onClick={handleCloseDropDown}>
+            <button
+              key={item.text}
+              onClick={handleCloseDropDown}
+              className='capitalize flex gap-3 items-center justify-start w-full m-4
+              text-mediumGray hover:text-almostBlack'
+            >
               {item.icon ? <img src={item?.icon} aria-hidden /> : null}
               {item.text}
             </button>
