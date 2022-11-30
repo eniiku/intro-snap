e0f94fd7dc8f974e34e90800ec0cd2498e13b7df
eniiku <enikuauth@gmail.com>
2022-11-30 21:11:51 +0100

    feat(Home): optimize homepage for responsiveness

--- src/container/Home.tsx
+++ src/container/Home.tsx
@@ -1,5 +1,6 @@
 import Navbar from '../components/Navbar/Navbar';
-import heroImage from '../assets/images/image-hero-mobile.png';
+import heroImageMobile from '../assets/images/image-hero-mobile.png';
+import heroImageDesktop from '../assets/images/image-hero-desktop.png';
 
 const trustedClients = [
   {
@@ -21,44 +22,61 @@ const trustedClients = [
 ];
 const Home = () => {
   return (
-    <section aria-label='Landing page for snap company'>
+    <section
+      aria-label='Landing page for snap company'
+      className='md:px-8 md:py-6'
+    >
       <div>
         <Navbar />
 
-        <img src={heroImage} role='presentation' />
+        <img src={heroImageMobile} role='presentation' className='md:hidden' />
       </div>
 
-      <div className='mt-12 flex flex-col items-center justify-center px-4'>
-        <div>
-          <h1 className='text-almostBlack font-bold text-4xl text-center'>
-            Make remote work
-          </h1>
-
-          <p className='text-center mt-4 text-mediumGray'>
-            Get your team in sync, no matter the location. Streamline processes,
-            create team rituals, and watch productivity soar.
-          </p>
-        </div>
-
-        <button
-          className='bg-almostBlack rounded-[1.3rem] py-4 px-6 font-bold 
-          text-almostWhite my-6'
-        >
-          Learn more
-        </button>
-
+      <div className='md:flex md:justify-center md:items-center md:px-[8vw] md:py-[2vh]'>
         <div
-          aria-label='list of clients that trust in our work'
-          className='flex justify-between items-center py-4'
+          className='mt-12 flex flex-col items-center justify-center px-4
+        md:grid md:grid-cols-2 md:gap-12'
         >
-          {trustedClients.map((client) => (
-            <img
-              key={client.name}
-              src={client.src}
-              alt={client.name}
-              className='w-[20%]'
-            />
-          ))}
+          <div className='md:flex md:flex-col md:justify-between md:items-start'>
+            <div>
+              <h1
+                className='text-almostBlack font-bold text-4xl md:text-7xl text-center
+              md:text-left'
+              >
+                Make remote work
+              </h1>
+
+              <p className='text-center md:text-left mt-4 text-mediumGray md:w-4/6'>
+                Get your team in sync, no matter the location. Streamline
+                processes, create team rituals, and watch productivity soar.
+              </p>
+            </div>
+
+            <button
+              className='bg-almostBlack rounded-[1.3rem] py-4 px-6 font-bold 
+          text-almostWhite my-6'
+            >
+              Learn more
+            </button>
+
+            <div
+              aria-label='list of clients that trust in our work'
+              className='flex justify-between items-center py-4'
+            >
+              {trustedClients.map((client) => (
+                <img
+                  key={client.name}
+                  src={client.src}
+                  alt={client.name}
+                  className='w-[20%] md:w-[15%]'
+                />
+              ))}
+            </div>
+          </div>
+
+          <div>
+            <img src={heroImageDesktop} role-='presentation' />
+          </div>
         </div>
       </div>
     </section>
